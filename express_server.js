3180f86df1418a1aba09814bde847e12cafc070b
CleoZhang <forest-zhang@hotmail.com>
2019-09-10 16:58:22 +0000

    Update URL database

--- express_server.js
+++ express_server.js
@@ -28,8 +28,8 @@ const generateRandomString = function(n) {
 };
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {longURL:"http://www.lighthouselabs.ca", userID: "aJ48lW"},
+  "9sm5xK": {longURL:"http://www.google.com", userID: "aJ48lW"}
 };
 
 const users = {
